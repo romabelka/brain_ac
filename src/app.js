6904c49f097efeb11daf67b603b0b81e6bc7a033
Roman Iakobchuk <r.iakobchuk@gmail.com>
2016-03-26 12:31:40 +0200

    add article component

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
-import SimpleComponent from './SImpleComponent'
+import Article from './Article'
 import React from 'react'
 import {render} from 'react-dom'
+import {articles} from './fixtures'
 
-
-render(<SimpleComponent />, document.getElementById('container'))
+render(<Article article = {articles[0]} />, document.getElementById('container'))
 
