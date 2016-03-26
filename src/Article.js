8b2de1a3392d91aa70e95ad9f3ad8e2e07dc38ce
Roman Iakobchuk <r.iakobchuk@gmail.com>
2016-03-26 14:31:34 +0200

    add reverse dataflow

--- src/Article.js
+++ src/Article.js
@@ -4,13 +4,20 @@ import toggleOpen from './HOC/toggleOpen'
 
 class Article extends Component {
     render() {
-        const {article : { title, text }, isOpen, toggleOpen} = this.props
+        const {article : { title, text }, isOpen, toggleOpen, isSelected} = this.props
+        const style = isSelected ? {color: 'red'} : null
         return (
-            <div>
+            <div style = {style}>
                 <h3 onClick = {toggleOpen}>{title}</h3>
+                <a href="#" onClick={this.handleClick}>select this article</a>
                 <Body text = {text} isOpen = {isOpen}/>
             </div>
         )
+    }
+
+    handleClick = (ev) => {
+        ev.preventDefault()
+        this.props.onClick()
     }
 }
 
--- src/ArticleList.js
+++ src/ArticleList.js
@@ -4,16 +4,36 @@ import JqueryComponent from './JqueryComponent'
 import { findDOMNode } from 'react-dom'
 
 class ArticleList extends Component {
+
+    static propTypes = {
+        articles: PropTypes.array.isRequired
+    }
+
+    state = {
+        selected: {}
+    }
+
     render() {
         const articleComponents = this.props.articles.map((article, index) =>
-            <li key={index}><Article article = {article} /></li>
+            <li key={index}>
+                <Article article = {article}
+                         onClick = {this.selectArticle(article.id)}
+                         isSelected={this.state.selected[article.id]} />
+            </li>
         )
+        console.log('---', this.state);
         return (
             <div>
                 <ul>{articleComponents}</ul>
                 <JqueryComponent ref = "customComponent" />
             </div>
         )
+    }
+
+    selectArticle = (id) => (ev) => {
+        this.setState({
+            selected: {...this.state.selected, [id]: true} //Object.assign({}, this.state.selected, {[id]: true})
+        })
     }
 
     componentDidMount() {
