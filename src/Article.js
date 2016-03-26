d85c5d3ff678d5fb33ef4bce695967dcd19fc8fa
Roman Iakobchuk <r.iakobchuk@gmail.com>
2016-03-26 12:52:39 +0200

    add state

--- src/Article.js
+++ src/Article.js
@@ -1,14 +1,25 @@
 import React, { Component, PropTypes } from 'react'
 
 class Article extends Component {
+    state = {
+        isOpen: false
+    }
+
     render() {
         const {article : { title, text }} = this.props
+        const body = this.state.isOpen ? <section>{text}</section> : null;
         return (
             <div>
-                <h3>{title}</h3>
-                <section>{text}</section>
+                <h3 onClick = {this.handleClick}>{title}</h3>
+                {body}
             </div>
         )
+    }
+
+    handleClick = (ev) => {
+        this.setState({
+            isOpen: !this.state.isOpen
+        })
     }
 }
 
