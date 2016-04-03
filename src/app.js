e7007aeaeab0b4bb3600dc6d6471a017dda437b4
Roman Iakobchuk <r.iakobchuk@gmail.com>
2016-04-03 12:00:30 +0300

    add AC

--- src/app.js
+++ src/app.js
@@ -3,9 +3,14 @@ import {render} from 'react-dom'
 //import Router from './Router'
 import store from './store'
 import Counter from './components/Counter'
+import { increment } from './AC/counter'
+
+function dispatchIncrement() {
+    store.dispatch(increment())
+}
 
 function rerender () {
-    render(<Counter {...store.getState()}/>, document.getElementById('container'))
+    render(<Counter {...store.getState()} increment = {dispatchIncrement}/>, document.getElementById('container'))
 }
 
 rerender()
--- src/components/Counter.js
+++ src/components/Counter.js
@@ -2,13 +2,16 @@ import React, { Component, PropTypes } from 'react'
 
 class Counter extends Component {
     static propTypes = {
-        count: PropTypes.number
+        count: PropTypes.number,
+        increment: PropTypes.func.isRequired
     };
 
     render() {
+        const { count, increment } = this.props
         return (
             <div>
-                <h1>{this.props.count}</h1>
+                <h1>{count}</h1>
+                <h3 onClick = {increment}>Increment me</h3>
             </div>
         )
     }
