9a24db706fe25db39d4f56e1f889c5976a6f9535
Roman Iakobchuk <r.iakobchuk@gmail.com>
2016-04-03 12:52:06 +0300

    add articles reducer

---
+++ src/reducer/articles.js
@@ -0,0 +1,18 @@
+import { DELETE_ARTICLE } from '../constants'
+import { articles } from '../fixtures'
+
+const defaultArticles = {
+    loading: false,
+    loaded: true,
+    entities: articles
+}
+
+export default (articles = defaultArticles, action) => {
+    const { type, data } = action
+
+    switch (type) {
+        case DELETE_ARTICLE: return Object.assign({}, articles, {entities: articles.entities.filter(id => id != data.id)})
+    }
+
+    return articles
+}
--- src/reducer/index.js
+++ src/reducer/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux'
 import counter from './counter'
+import articles from './articles'
 
 export default combineReducers({
-    count: counter
+    count: counter,
+    articles
 })
 
